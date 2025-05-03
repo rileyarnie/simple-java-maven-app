66b74cf24dcace1f31d66b62154de3f0c5bc17ec
rileyarnie <arnold.ril3y@gmail.com>
2025-05-03 16:34:57 +0300

    use a shared jenkins library

--- jenkins/scripts/script.groovy
+++ jenkins/scripts/script.groovy
@@ -1,20 +1,3 @@
-def buildApp(){
-    sh 'mvn -B -DskipTests clean package'
-}
-
-def testApp(){
-    sh 'mvn test'
-}
-
-def buildAndPushDockerImage(){
-    echo "building the docker image"
-    withCredentials([usernamePassword(credentialsId: "docker-hub-repo", passwordVariable: 'PASS', usernameVariable: "USER" )]){
-        sh 'docker build -t arnoldkibira/simple-java-maven-app:1.2 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh "docker push arnoldkibira/simple-java-maven-app:1.2"
-    }
-}
-
 def deliverApp(){
    sh 'jenkins/scripts/deliver.sh'
 }
